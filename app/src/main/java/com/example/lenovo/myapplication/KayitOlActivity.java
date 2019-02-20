764cf58219f910b13022d23fbdeddb3b5a8fc319
Ezgi Iscioglu <g151210012@sakarya.edu.tr>
2019-02-20 23:17:31 +0300

    Kayıt ol,giriş yap eklendi

---
+++ app/src/main/java/com/example/lenovo/myapplication/KayitOlActivity.java
@@ -0,0 +1,35 @@
+package com.example.lenovo.myapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class KayitOlActivity extends AppCompatActivity {
+    EditText etAd,etSoyad,etEmail,etSifre;
+    Button btnKayitOl;
+    Context context;
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kayit);
+
+        etAd= (EditText) findViewById(R.id.etAd);
+        etSoyad= (EditText) findViewById(R.id.etSoyad);
+        etEmail= (EditText) findViewById(R.id.etEmail);
+        etSifre= (EditText) findViewById(R.id.etSifre);
+        btnKayitOl= (Button) findViewById(R.id.btnKayitOl) ;
+
+        btnKayitOl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(KayitOlActivity.this,OturumAcActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/lenovo/myapplication/OturumAcActivity.java
@@ -0,0 +1,31 @@
+package com.example.lenovo.myapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class OturumAcActivity extends AppCompatActivity {
+    EditText etEmail,etSifre;
+    Button btnOturumAc;
+    Context context;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.oturum);
+        etEmail= (EditText) findViewById(R.id.etEmail);
+        etSifre= (EditText) findViewById(R.id.etSifre);
+        btnOturumAc= (Button) findViewById(R.id.btnOturumAc) ;
+        btnOturumAc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(OturumAcActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
