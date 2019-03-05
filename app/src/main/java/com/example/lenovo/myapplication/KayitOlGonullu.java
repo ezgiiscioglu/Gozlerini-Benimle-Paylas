cca1959db6c5bd418cac474ecbc1164545e47afe
ezgiiscioglu <g151210012@sakarya.edu.tr>
2019-03-06 00:24:07 +0300

    Düzenlendi.

--- app/src/main/java/com/example/lenovo/myapplication/KayitOlGonullu.java
+++ app/src/main/java/com/example/lenovo/myapplication/KayitOlGonullu.java
@@ -3,13 +3,23 @@ package com.example.lenovo.myapplication;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class KayitOlGonullu extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
     EditText etAd,etSoyad,etEmail,etSifre;
     Button btnKayitOl;
     Context context;
@@ -17,18 +27,25 @@ public class KayitOlGonullu extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.kayit_ol_gonullu);
-        etAd= (EditText) findViewById(R.id.etAd);
-        etSoyad= (EditText) findViewById(R.id.etSoyad);
-        etEmail= (EditText) findViewById(R.id.etEmail);
-        etSifre= (EditText) findViewById(R.id.etSifre);
-        btnKayitOl= (Button) findViewById(R.id.btnKayitOl) ;
 
-        btnKayitOl.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+        etEmail = findViewById(R.id.etEmail);
+        etSifre = findViewById(R.id.etSifre);
+    }
+    public void KayitOlGonullu(View view) {
+        mAuth.createUserWithEmailAndPassword(etEmail.getText().toString(), etSifre.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Toast.makeText(KayitOlGonullu.this, "Kayit Başarıyla Tamamlandı.", Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(KayitOlGonullu.this, OturumAcGonullu.class);
+                        startActivity(i);
+                    }
+
+                }).addOnFailureListener(this, new OnFailureListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(KayitOlGonullu.this,OturumAcGonullu.class);
-                startActivity(intent);
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(KayitOlGonullu.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
             }
         });
-    }
-}
+    }}
--- app/src/main/java/com/example/lenovo/myapplication/OturumAcGonullu.java
+++ app/src/main/java/com/example/lenovo/myapplication/OturumAcGonullu.java
@@ -3,6 +3,7 @@ package com.example.lenovo.myapplication;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,7 +11,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class OturumAcGonullu extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
     Button btnOturumAc;
     EditText etEmail,etSifre;
     ImageView imageView;
@@ -20,17 +29,33 @@ public class OturumAcGonullu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.oturum_ac_gonullu);
 
-        etEmail= (EditText) findViewById(R.id.etEmail);
-        etSifre= (EditText) findViewById(R.id.etSifre);
+        mAuth = FirebaseAuth.getInstance();
+        etEmail = findViewById(R.id.etEmail);
+        etSifre = findViewById(R.id.etSifre);
+
+
         imageView=(ImageView)findViewById(R.id.imageView);
         btnOturumAc= (Button) findViewById(R.id.btnOturumAc) ;
+    }
 
-        btnOturumAc.setOnClickListener(new View.OnClickListener() {
+    public void OturumAcGonullu(View view) {
+        mAuth.signInWithEmailAndPassword(etEmail.getText().toString(), etSifre.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(OturumAcGonullu.this, GonulluKullaniciEkrani.class);
+                            startActivity(intent);
+
+
+                        }
+                    }
+                }).addOnFailureListener(this, new OnFailureListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(OturumAcGonullu.this,GonulluKullaniciEkrani.class);
-                startActivity(intent);
+            public void onFailure(@NonNull Exception e) {
+
             }
         });
+
     }
 }
